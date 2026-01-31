a8eb48503f46cfb6fec2af6af2fb1c1b1813de45
Jaclin Clarck <golejaclin82@gmail.com>
2026-01-31 04:41:42 +0100

    404 and main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
 
+const params = new URLSearchParams(window.location.search)
+const redirect = params.get("redirect")
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect)
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
